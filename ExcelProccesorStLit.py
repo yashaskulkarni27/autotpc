00176dd3d13da239e87a2ea14852ef4c4852c296
yashas <yashasmkulkarni@gmail.com>
2024-06-06 16:01:25 +0530

    initial commit

---
+++ ExcelProccesorStLit.py
@@ -0,0 +1,70 @@
+import streamlit as st
+from ExcelProcessor import ExcelProcessor
+from io import BytesIO
+
+def main():
+    st.title("Excel Processing App")
+    st.sidebar.title("Input Parameters")
+
+    # File uploader for Excel file
+    uploaded_file = st.sidebar.file_uploader("Upload Excel file", type=["xlsx"])
+
+    if uploaded_file:
+        st.sidebar.header("Cutoff Percentages")
+        cutoff_10th = st.sidebar.slider("10th Percentage Cutoff", min_value=0, max_value=100, value=70)
+        cutoff_12th = st.sidebar.slider("12th Percentage Cutoff", min_value=0, max_value=100, value=70)
+        cutoff_btech_cgpa = st.sidebar.slider("BTech CGPA Cutoff", min_value=0.0, max_value=10.0, value=6.0)
+        cutoff_live_kt = st.sidebar.slider("Live KT Cutoff", min_value=0, max_value=10, value=0)
+        cutoff_drop = st.sidebar.slider("Drop Cutoff", min_value=0, max_value=10, value=0)
+        cutoff_gap = st.sidebar.slider("Gap Cutoff", min_value=0, max_value=10, value=2)
+
+        if st.sidebar.button("Process"):
+            # Instantiate ExcelProcessor with input parameters
+            processor = ExcelProcessor(uploaded_file, cutoff_10th, cutoff_12th, cutoff_btech_cgpa,
+                                       cutoff_live_kt, cutoff_drop, cutoff_gap)
+
+            # Process the Excel file
+            processor.clean_data()
+            removed_data = processor.check_data()
+            processor.remove_columns()
+            processor.basic_editing()
+            ask_percentage = True  # Set to False if CGPA is to be kept
+            processor.remove_unwanted_data(ask_percentage)
+            preferred_order = [
+                'Full Name', 'Personal Email ID', 'CollegeRollNo', 'Contact No', 'Gender',
+                'Branch', 'BTech Major Course', 'College Name', '10th Percentage',
+                '10th Year of Passing', '12th/Diploma', '12th/ Diploma Percentage',
+                '12th/ Diploma Year of Passing', 'Degree', 'BTech CGPA', 'BTech Percentage',
+                'Batch', 'Resume'
+            ]
+            processor.sort_columns(preferred_order)
+            processor.add_serial_column()
+            processor.format_sheet()
+            processor.rename_sheet('RAIT')
+            processor.fill_empty_cells_with_na()
+            # processor.adjust_column_widths()
+
+            # Save processed data and removed data
+            processed_file_path = "processed_file.xlsx"
+            processor.save_data(processed_file_path, removed_data)
+            file = processor.adjust_column_widths(processed_file_path, "processed_file.xlsx").save("processed_file.xlsx")
+            st.success("Processing completed successfully!")
+
+            # Provide download link for processed file
+            # st.markdown(f"Download processed file [here](file)", unsafe_allow_html=True)
+            file_path = 'processed_file.xlsx'
+            with open(file_path, 'rb') as file:
+                file_data = file.read()
+            file_buffer = BytesIO(file_data)
+
+            # Provide a download button
+            st.download_button(
+                label="Download processed file",
+                data=file_buffer,
+                file_name='processed_with_widths.xlsx',
+                mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+            )
+
+
+if __name__ == "__main__":
+    main()
---
+++ ExcelProcessor.py
@@ -0,0 +1,186 @@
+import os
+import pandas as pd
+from openpyxl import load_workbook, Workbook
+from openpyxl.styles import Alignment
+
+class ExcelProcessor:
+    def __init__(self, file_path, cutoff_10th, cutoff_12th, cutoff_btech_cgpa,
+                 cutoff_live_kt, cutoff_drop, cutoff_gap):
+        self.file_path = file_path
+        self.cutoff_10th = cutoff_10th
+        self.cutoff_12th = cutoff_12th
+        self.cutoff_btech_cgpa = cutoff_btech_cgpa
+        self.cutoff_live_kt = cutoff_live_kt
+        self.cutoff_drop = cutoff_drop
+        self.cutoff_gap = cutoff_gap
+        self.df = pd.read_excel(self.file_path, sheet_name='Form responses 1')
+
+    def clean_data(self):
+        self.df.iloc[:, 0] = self.df.iloc[:, 0].astype(str).str.replace(' ', '')
+        self.df.drop_duplicates(subset=self.df.columns[0], inplace=True)
+        self.df.sort_values(by=self.df.columns[0], inplace=True)
+
+    def check_eligibility_Percentage(self, column, cutoff, removed_df):
+        removed = self.df[self.df[column] < cutoff]
+        remaining = self.df[self.df[column] >= cutoff]
+        removed_df = pd.concat([removed_df, removed])
+        return remaining, removed_df
+
+    def check_eligibility_Diff(self, column, cutoff, removed_df):
+        removed = self.df[self.df[column] > cutoff]
+        remaining = self.df[self.df[column] <= cutoff]
+        removed_df = pd.concat([removed_df, removed])
+        return remaining, removed_df
+
+    def check_year_format(self, column_name):
+        invalid_year_format = ~self.df[column_name].astype(str).str.match(r'^\d{4}$')
+        if invalid_year_format.any():
+            print(f"Invalid {column_name} format at rows:")
+            print(self.df[invalid_year_format])
+
+    def check_data(self):
+        removed_df = pd.DataFrame(columns=self.df.columns)
+        self.df, removed_df = self.check_eligibility_Percentage('10th Percentage', self.cutoff_10th, removed_df)
+        self.check_year_format('10th Year of Passing')
+        self.df, removed_df = self.check_eligibility_Percentage('12th/ Diploma Percentage', self.cutoff_12th, removed_df)
+        self.check_year_format('12th/ Diploma Year of Passing')
+        self.df, removed_df = self.check_eligibility_Percentage('BTech CGPA', self.cutoff_btech_cgpa, removed_df)
+        self.df, removed_df = self.check_eligibility_Diff('Live KT', self.cutoff_live_kt, removed_df)
+        self.df, removed_df = self.check_eligibility_Diff('Drop', self.cutoff_drop, removed_df)
+        self.df, removed_df = self.check_eligibility_Diff('Gap', self.cutoff_gap, removed_df)
+        return removed_df
+
+    def remove_columns(self):
+        columns_to_remove = ['Timestamp', 'Email address', 'College Roll No', 'Job Role', 'Minor Course', 'Live KT',
+                             'Dead KT', 'Drop', 'Gap', 'Are you placed?', 'Company Name 1', 'Company Name 2',
+                             'Statement of Acknowledgement']
+        columns_to_remove = [col for col in columns_to_remove if col in self.df.columns]
+        self.df.drop(columns_to_remove, axis=1, inplace=True)
+
+    def basic_editing(self):
+        self.df.iloc[:, 1] = self.df.iloc[:, 1].str.strip()
+
+    def remove_unwanted_data(self, percentage):
+        if percentage:
+            self.df.drop('BTech CGPA', axis=1, inplace=True)
+        else:
+            self.df.drop('BTech Percentage', axis=1, inplace=True)
+
+    def format_sheet(self):
+        self.df.style.set_properties(**{
+            'font-family': 'Arial',
+            'font-size': '10pt',
+            'text-align': 'center',
+        }).set_table_styles(
+            [{'selector': 'th', 'props': [('font-weight', 'bold')]}]
+        )
+
+    def rename_sheet(self, sheet_name):
+        self.df.rename(columns={'Full Name': 'Name'}, inplace=True)
+
+    def fill_empty_cells_with_na(self):
+        self.df.fillna('NA', inplace=True)
+
+    def sort_columns(self, preferred_order):
+        existing_columns = list(self.df.columns)
+        ordered_columns = [col for col in preferred_order if col in existing_columns] + \
+                          [col for col in existing_columns if col not in preferred_order]
+        self.df = self.df[ordered_columns]
+        self.df = self.df.sort_values(by='Full Name', ascending=True)
+
+    def add_serial_column(self):
+        self.df.insert(0, 'Sr No', range(1, len(self.df) + 1))
+
+    def save_data(self, output_file, removed_df, sheet_name='RAIT'):
+        # Save the DataFrame to an Excel file with hyperlinks preserved
+        full_otPath = os.path.join(os.getcwd(), output_file)
+
+        with pd.ExcelWriter(full_otPath, engine='openpyxl') as writer:
+            self.df.to_excel(writer, sheet_name=sheet_name, index=False)
+            removed_df.to_excel(writer, sheet_name='removed', index=False)
+
+        wb = load_workbook(full_otPath)
+        ws = wb[sheet_name]
+
+        # Reapply hyperlinks to the Resume column
+        resume_col_idx = self.df.columns.get_loc('Resume') + 1
+        for idx, url in enumerate(self.df['Resume'], start=2):  # Data starts from row 2
+            if pd.notna(url):
+                ws.cell(row=idx, column=resume_col_idx).hyperlink = url
+                ws.cell(row=idx, column=resume_col_idx).style = "Hyperlink"
+
+        wb.save(full_otPath)
+
+    def adjust_column_widths(self, input_file, output_file):
+        wb = load_workbook(input_file)
+        ws = wb.active
+
+        for column in ws.columns:
+            max_length = 0
+            column_letter = column[0].column_letter
+            for cell in column:
+                try:
+                    if len(str(cell.value)) > max_length:
+                        max_length = len(str(cell.value))
+
+                    cell.alignment = Alignment(horizontal='center')
+
+                    if isinstance(cell.value, (int, float)):
+                        cell.alignment = Alignment(horizontal='center')
+
+                except TypeError:
+                    pass
+            ws.column_dimensions[column_letter].width = max_length + 2
+
+        return wb
+
+# Define the cut-off percentages
+CutOff10th = 70
+CutOff12th = 70
+CutOffBTechCGPA = 6
+CutOffBTechPercentage = 60
+CutOffLiveKT = 0
+CutOffDrop = 0
+CutOffGap = 2
+
+# Instantiate the ExcelProcessor class
+processor = ExcelProcessor('Wua.xlsx', CutOff10th, CutOff12th, CutOffBTechCGPA,
+                           CutOffLiveKT, CutOffDrop, CutOffGap)
+
+# Clean the data
+processor.clean_data()
+
+# Check eligibility and format the data
+removed_data = processor.check_data()
+processor.remove_columns()
+processor.basic_editing()
+
+# Assuming user input for whether to keep percentage or CGPA
+ask_percentage = True  # Set to False if CGPA is to be kept
+processor.remove_unwanted_data(ask_percentage)
+
+# Sort the columns based on a preferred order
+preferred_order = [
+    'Full Name', 'Personal Email ID', 'CollegeRollNo', 'Contact No', 'Gender',
+    'Branch', 'BTech Major Course', 'College Name', '10th Percentage',
+    '10th Year of Passing', '12th/Diploma', '12th/ Diploma Percentage',
+    '12th/ Diploma Year of Passing', 'Degree', 'BTech CGPA', 'BTech Percentage',
+    'Batch', 'Resume'
+]
+processor.sort_columns(preferred_order)
+
+# Add a new serial column
+processor.add_serial_column()
+
+# Format the sheet
+processor.format_sheet()
+
+# Rename sheet to RAIT
+processor.rename_sheet('RAIT')
+processor.fill_empty_cells_with_na()
+
+# Save the cleaned data and removed data to a new Excel file
+processor.save_data('processed_file.xlsx', removed_data)
+
+# Adjust column widths based on the length of the longest text in each column for all cells
+processor.adjust_column_widths('processed_file.xlsx', 'processed_with_widths.xlsx')
